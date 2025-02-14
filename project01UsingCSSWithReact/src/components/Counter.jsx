6c1883132a374cc02a0a9988f5363bd4ef9d7e0e
Amarildo Golloshi <golloshiamarildo@gmail.com>
2025-02-14 16:46:50 +0000

    Using CSS with React

---
+++ project01UsingCSSWithReact/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const pageTitle = document.title;
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect( () => {
+        count && (document.title = `${pageTitle}--${count}`)
+    } )
+
+    return (
+        <button onClick={() => setCount(count + 1) }>
+            Click Me {count === 0 ? `` : `${count} times`}
+        </button>
+    )
+}
+
+export default Counter;
